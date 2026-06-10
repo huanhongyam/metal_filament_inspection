c5fa4442a91ac9fcacc3d1c50fb88b374069626e
huanhongyam <shendie402170@126.com>
2026-06-10 13:27:39 +0800

    更新配置文件指向服务器

---
+++ src/main/java/com/kunpeng/metal_filament_inspection/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.kunpeng.metal_filament_inspection.controller;
+
+import com.kunpeng.metal_filament_inspection.domain.dto.Result;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/user")
+public class UserController {
+
+}
